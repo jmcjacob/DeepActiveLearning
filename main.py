94af37c55722c37df1a314af4b5ba19715be7cc1
Jacob Carse <jacob.carse@gmail.com>
2017-10-03 14:17:07 +0100

    Added data class

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import csv
 import numpy as np
 import tensorflow as tf
 
 
 class Model:
-    def __init__(self):
-        self.num_input = 784
-        self.num_classes = 10
+    def __init__(self, num_input, num_classes):
+        self.num_input = num_input
+        self.num_classes = num_classes
         self.X = tf.placeholder("float", [None, self.num_input])
         self.Y = tf.placeholder("float", [None, self.num_classes])
         self.model = self.create_model()
@@ -23,8 +24,8 @@ class Model:
             'out': tf.Variable(tf.random_normal([self.num_classes]))
         }
 
-        layer_1 = tf.add(tf.matmul(self.X, weights['h1']), biases['b1'])
-        layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        layer_1 = tf.nn.relu(tf.add(tf.matmul(self.X, weights['h1']), biases['b1']))
+        layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, weights['h2']), biases['b2']))
         out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
         return out_layer
 
@@ -33,6 +34,7 @@ class Model:
         optimizer = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
         correct_pred = tf.equal(tf.argmax(tf.nn.softmax(self.model), 1), tf.argmax(self.Y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
         init = tf.global_variables_initializer()
         saver = tf.train.Saver()
 
@@ -48,7 +50,7 @@ class Model:
                                             feed_dict={self.X: data[batch], self.Y: labels[batch]})
                     avg_loss += cost
                     avg_acc += acc
-                print('Epoch: ' + str(epoch) + ' Accuracy: ' + '{:.3f}'.format(avg_acc/num_batches)
+                print('Epoch: ' + str(epoch) + ' Accuracy: ' + '{:.3f}'.format(1 - (avg_acc/num_batches))
                       + ' Loss: ' + '{:.4f}'.format(avg_loss/num_batches))
 
             print('Optimization Finished!')
@@ -59,9 +61,36 @@ class Model:
         print("Model saved in file: %s" % save_path)
 
 
+class MNIST_Data:
+    def __init__(self, train_file, test_file):
+        train_x, train_y = [], []
+        test_x, test_y = [], []
+        with open(train_file) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                label = np.zeros(10)
+                label[int(row[0])] = 1
+                train_y.append(label)
+                train_x.append(list(map(int, row[1:])))
+        print('Training Data Loaded from file')
+
+        with open(test_file) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                label = np.zeros(10)
+                label[int(row[0])] = 1
+                test_y.append(label)
+                test_x.append(list(map(int, row[1:])))
+        print('Testing Data Loaded from file')
+
+        self.train_x, self.train_y = np.asarray(train_x), np.asarray(train_y)
+        self.test_x, self.test_y = np.asarray(test_x), np.asarray(test_y)
+
+
 def main():
-    model = Model()
-    print(model)
+    model = Model(784, 10)
+    data = MNIST_Data('mnist_train.csv', 'mnist_test.csv')
+    model.train(0, data.train_x, data.train_y, 100, data.test_x, data.test_y)
 
 
 if __name__ == '__main__':
