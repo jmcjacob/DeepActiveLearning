6f1a4cee4175a04654967654639cc1e8991dd4b2
Jacob Carse <jacob.carse@gmail.com>
2017-10-05 12:58:26 +0100

    Added a weighted cost function This will deal with the unbalances during data selection.

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import os
 import csv
 import numpy as np
 import tensorflow as tf
+import keras.backend as K
+from itertools import product
+from collections import Counter
 from sklearn.model_selection import train_test_split
+
+
+use_weights = True
 
 
 class Model:
@@ -12,7 +18,6 @@ class Model:
         self.X = tf.placeholder("float", [None, self.num_input])
         self.Y = tf.placeholder("float", [None, self.num_classes])
         self.model = self.create_model()
-        print('Model has been created')
 
     def create_model(self):
         self.weights = {
@@ -31,11 +36,28 @@ class Model:
         out_layer = tf.matmul(layer_2, self.weights['out']) + self.biases['out']
         return out_layer
 
-    def train(self, version, data, labels, batch_size, test_data, test_labels):
+    def weighted_crossentropy(self, y_true, y_pred, weights):
+        nb_cl = len(weights)
+        final_mask = K.zeros_like(y_pred[..., 0])
+        y_pred_max = K.max(y_pred, axis=-1)
+        y_pred_max = K.expand_dims(y_pred_max, axis=-1)
+        y_pred_max_mat = K.equal(y_pred, y_pred_max)
+        for c_p, c_t in product(range(nb_cl), range(nb_cl)):
+            w = K.cast(weights[c_t, c_p], K.floatx())
+            y_p = K.cast(y_pred_max_mat[..., c_p], K.floatx())
+            y_t = K.cast(y_pred_max_mat[..., c_t], K.floatx())
+            final_mask += w * y_p * y_t
+        return tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred) * final_mask
+
+    def train(self, version, data, labels, batch_size, test_data, test_labels, weights=np.ones((0, 0))):
         beta = 0.5
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.model, labels=self.Y) +
-                              beta + tf.nn.l2_loss(self.weights['h2']) + beta + tf.nn.l2_loss(self.biases['b2']) +
-                              beta + tf.nn.l2_loss(self.weights['out']) + beta + tf.nn.l2_loss(self.biases['out']))
+        if use_weights:
+            loss = self.weighted_crossentropy(y_true=self.Y, y_pred=self.model, weights=weights)
+        else:
+            loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.model, labels=self.Y)
+        loss += beta + tf.nn.l2_loss(self.weights['h2']) + beta + tf.nn.l2_loss(self.biases['b2']) + \
+                beta + tf.nn.l2_loss(self.weights['out']) + beta + tf.nn.l2_loss(self.biases['out'])
+        loss = tf.reduce_mean(loss)
         optimizer = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
         correct_pred = tf.equal(tf.argmax(self.model, 1), tf.argmax(self.Y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
@@ -138,6 +160,25 @@ class MNIST_Data:
         self.train_x = training_data_x
         self.train_y = training_data_y
 
+    def get_weights(self, smooth_factor=0):
+        temp_y = []
+        for i in self.train_y:
+            temp_y.append(np.argmax(i))
+        counter = Counter(temp_y)
+        if smooth_factor > 0:
+            p = max(counter.values()) * smooth_factor
+            for k in counter.keys():
+                counter[k] += p
+        majority = max(counter.values())
+        weights = {cls: float(majority / count) for cls, count in counter.items()}
+
+        nb_cl = len(weights)
+        final_weights = np.ones((nb_cl, nb_cl))
+        for class_idx, class_weight in weights.items():
+            final_weights[0][class_idx] = class_weight
+            final_weights[class_idx][0] = class_weight
+        return final_weights
+
 
 def main():
     model = Model(784, 10)
@@ -145,16 +186,22 @@ def main():
     original_size = len(data.train_x)
     print('\nOriginal Size: ' + str(original_size))
     accuracies = []
-    accuracies.append(model.train(0, data.train_x, data.train_y, 100, data.test_x, data.test_y))
+    if use_weights:
+        accuracies.append(model.train(0, data.train_x, data.train_y, 100, data.test_x, data.test_y, data.get_weights()))
+    else:
+        accuracies.append(model.train(0, data.train_x, data.train_y, 100, data.test_x, data.test_y))
     data.reduce_data(0.99)
     for i in range(1, 11):
         new_model = Model(784, 10)
         print('\nVersion ' + str(i) + ' Size: ' + str(len(data.train_x)))
-        accuracies.append(new_model.train(i, data.train_x, data.train_y, 100, data.test_x, data.test_y))
-        predictions = new_model.predict(i, data.predict_x)
+        if use_weights:
+            accuracies.append(new_model.train(i, data.train_x, data.train_y, 100, data.test_x, data.test_y,
+                                          data.get_weights()))
+        else:
+            accuracies.append(new_model.train(i, data.train_x, data.train_y, 100, data.test_x, data.test_y))
+        predictions = model.predict(i, data.predict_x)
         data.increase_data(predictions, int(original_size * 0.01))
     print(accuracies)
-    #model.test(i, data.test_x, data.test_y)
 
 
 if __name__ == '__main__':
